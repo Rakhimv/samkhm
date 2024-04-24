4462049d5a6528bd28e3bc91e1af4ca11698296b
rakhimv <azizbekrakhimv@gmail.com>
2024-04-25 00:56:08 +0500

    v2

--- src/Components/RatingBox.tsx
+++ src/Components/RatingBox.tsx
@@ -1,4 +1,4 @@
-import { Button, Input, Textarea } from '@nextui-org/react'
+import { Button, Card, Input, Textarea, User } from '@nextui-org/react'
 // import Zoom from 'react-medium-image-zoom'
 // import 'react-medium-image-zoom/dist/styles.css'
 import { useEffect, useState } from 'react'
@@ -7,6 +7,7 @@ import 'aos/dist/aos.css';
 import { useForm } from "react-hook-form"
 import axios from "axios"
 import { Rating } from '@mui/material';
+import { formatTimeDetal } from '../Utils/Utils';
 
 
 function RatingBox() {
@@ -14,13 +15,19 @@ function RatingBox() {
     const { register, handleSubmit } = useForm();
     const [ratings, setRatings] = useState<any>(null)
 
-    console.log(setValue);
-    
+
+    const [loadcom, setLoadcom] = useState<boolean>(false)
+
+
     async function getRatings() {
         try {
             const req = await axios.get('https://samkhm34-default-rtdb.asia-southeast1.firebasedatabase.app/rating.json')
             if (req.data) {
-                const ratingsArray = Object.keys(req.data).map(key => req.data[key]);
+                const ratingsArray = Object.keys(req.data).map(key => {
+                    const newsItem = req.data[key];
+                    newsItem.key = key;
+                    return newsItem;
+                }).reverse();
                 setRatings(ratingsArray)
                 console.log(ratingsArray);
             }
@@ -43,51 +50,89 @@ function RatingBox() {
 
 
 
-    const submitRating = (data: any) => {
+    async function submitR(data: any) {
+        setLoadcom(true)
         try {
-            data.rating = value;
+
+            if (value == 0) {
+                data.rating = 5;
+            } else {
+                data.rating = value;
+            }
+            data.ctime = Date.now()
             console.log(data);
-            const req = axios.post('https://samkhm34-default-rtdb.asia-southeast1.firebasedatabase.app/rating.json', data)
+            const req = await axios.post('https://samkhm34-default-rtdb.asia-southeast1.firebasedatabase.app/rating.json', data)
             console.log(req);
-            
+            localStorage.setItem('rating', data.rating)
+            setLoadcom(false)
+            getRatings()
         } catch {
             console.log('Error');
-
+            setLoadcom(false)
         }
     }
 
 
     return (
         <>
-            <div className='w-full flex justify-center py-[50px] pt-[20px] mt-[50px] mb-[0px] noxs1000:p-[20px]' data-aos="fade-up">
+
+            <div className='w-full flex justify-center py-[50px] pt-[20px] mt-[50px] mb-[0px] noxs1000:p-[20px]'>
                 <div className='container max-w-[900px] '>
-
-                    <form onSubmit={handleSubmit(submitRating)}>
-                        <Rating />
-                        <Input
-                            variant='bordered'
-                            label="Ваше имя"
-                            {...register('name', { required: true })}
-                        />
-                        <Textarea
-                            variant='bordered'
-                            placeholder="Отзыв"
-                            {...register('text', { required: true })}
-                        />
-                        <Button color='primary' type='button' onClick={() => window.location.href = 'https://www.roblox.com/download/client?os=win'} variant='solid'>Отправить</Button>
-                    </form>
+                    <div className={`py-[20px] flex justify-between noxs658:flex-col noxs1000:gap-[20px] items-center`}>
+                        <p className="text-2xl font-bold">Отзывы</p>
+                    </div>
 
 
 
-                    {ratings &&
+                    <div className='comms py-[50px] pt-[0px] gap-[20px] flex flex-col'>
+                        {ratings &&
+                            ratings.map((item: any, index: number) =>
+                                <Card onContextMenu={(e: any) => {
+                                    e.preventDefault();
+                                    if (localStorage.getItem('admin')) {
+                                        let promt = confirm('Удалить комментарий?')
+                                        if (promt) {
+                                            axios.delete('https://samkhm34-default-rtdb.asia-southeast1.firebasedatabase.app/rating/' + item.key + '.json')
+                                            getRatings()
+                                        }
+                                    }
+                                }} key={index} className='p-[20px] flex gap-[20px] '>
+                                    <div className='flex gap-[20px] items-start justify-between noxs658:flex-col-reverse'>
+                                        <User name={item.name} className='flex justify-start' description={formatTimeDetal(item.ctime)} />
+                                        <Rating size='medium' readOnly value={item.rating} />
+                                    </div>
+                                    <p>{item.text}</p>
+                                </Card>
+                            )
+                        }
+                    </div>
 
-                        <div>
-                            {ratings.map((item: any, index: number) =>
-                                <p key={index}>{item.name}</p>
-                            )}
-                        </div>
+                    {!localStorage.getItem('rating') &&
+                        <form onSubmit={handleSubmit(submitR)} className='flex flex-col mb-[50px] gap-[20px]'>
+                            <div className='flex noxs658:justify-center'>
+                                <Rating value={value}
+                                    onChange={(_, newValue) => {
+                                        setValue(newValue);
+                                    }} size='large' />
+                            </div>
+                            <Input
+                                variant='bordered'
+                                label="Ваше имя"
+                                {...register('name', { required: true })}
+                            />
+                            <Textarea
+                                variant='bordered'
+                                placeholder="Отзыв"
+                                {...register('text', { required: true })}
+                            />
+                            <Button isLoading={loadcom} color='primary' size='lg' type='submit' variant='solid'>Отправить</Button>
+
+
+                        </form>
 
                     }
+
+
                 </div>
             </div>
         </>
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -29,7 +29,7 @@ const Home = () => {
 
 
             <div className="w-full mb-[150px] mt-[50px] flex justify-center">
-                <div className='container max-w-[900px] flex flex-col gap-[20px] noxs658:p-[20px]'>
+                <div className='container max-w-[900px] flex flex-col gap-[20px] noxs1000:p-[20px]'>
                     <div className={`py-[20px] flex justify-between noxs658:flex-col noxs1000:gap-[20px] items-center`}>
                         <p className="text-2xl font-bold noxs658:text-center">Часто задаваемые вопросы</p>
                     </div>
