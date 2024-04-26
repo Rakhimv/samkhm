5e12e985cfef3e119c56b76f0b8b25199b153908
rakhimv <azizbekrakhimv@gmail.com>
2024-04-26 20:58:49 +0500

    finish

--- src/Components/Accordions.tsx
+++ src/Components/Accordions.tsx
@@ -1,21 +1,32 @@
 
 
-import {Accordion, AccordionItem} from "@nextui-org/react";
-
+import { Accordion, AccordionItem } from "@nextui-org/react";
+import { getLang } from "../Utils/Utils";
+import HelpIcon from '@mui/icons-material/Help';
 export default function Accordions() {
-  const defaultContent =
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+  let tit1 = getLang() ? 'На каком языке осуществляется обучение в профессиональной школе г. Самарканд' : `Samarqand shahridagi kasb-hunar maktabida qaysi tilda ta'lim olib boriladi?`
+  let con1 = getLang() ? 'Занятия проводятся на узбекском и русском языках' : `Darslar o'zbek va rus tillarida olib boriladi`
 
+
+  let tit2 = getLang() ? 'Каковы условия для поступления в вашу школу?' : 'Maktabingizga kirish uchun qanday shartlar mavjud?'
+  let con2 = getLang() ? 'Для поступления в нашу школу необходимо предоставить аттестат, подтверждающий завершение среднего образования. Мы также принимаем учеников только из города Самарканд или с пропиской в Самарканде.' : `
+  Maqtobimizga kirish uchun orta ma'lumotni tug'atuvchi attestat kerak. Biz Samarqand shahridan faqat yoki Samarqandda royhatga kirgan talabarni qabul qilamiz.
+`
+
+  let tit3 = getLang() ? 'Каков срок обучения в вашей профессиональной школе?' : `Sizning kasb hunar maktabingizda o'qish davomiyligi qancha?`
+  let con3 = getLang() ? 'Обучение в нашей профессиональной школе длится 2 года. По завершении программы обучения выдаётся сертификат, подтверждающий успешное завершение обучения.' : `
+  Bizning kasb hunar maktabimizda o'qish 2 yil davom etadi. O'qish dasturini yakunlagandan so'ng, o'qishni muvaffaqiyatli tugatganligingizni tasdiqlovchi sertifikat beriladi.`
+  
   return (
-    <Accordion variant="splitted" className="!px-0" style={{gap: 15}} >
-      <AccordionItem key="1" aria-label="Accordion 1" title="Accordion 1">
-        {defaultContent}
+    <Accordion variant="splitted" defaultSelectedKeys={'1'} className="!px-0" style={{ gap: 15 }} >
+      <AccordionItem key="1" title={tit1} startContent={<HelpIcon color="primary" />}>
+        <p className="mb-[20px]">{con1}</p>
       </AccordionItem>
-      <AccordionItem key="2" aria-label="Accordion 2" title="Accordion 2">
-        {defaultContent}
+      <AccordionItem key="2" title={tit2} startContent={<HelpIcon color="primary" />}>
+        <p className="mb-[20px]">{con2}</p>
       </AccordionItem>
-      <AccordionItem key="3" aria-label="Accordion 3" title="Accordion 3">
-        {defaultContent}
+      <AccordionItem key="3" title={tit3} startContent={<HelpIcon color="primary" />}>
+        <p className="mb-[20px]">{con3}</p>
       </AccordionItem>
     </Accordion>
   );
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -67,7 +67,7 @@ const Home = () => {
 
 
 
-            <div className="w-full mb-[150px] mt-[50px] flex justify-center">
+            <div className="w-full mb-[80px] mt-[50px] flex justify-center">
                 <div className='container max-w-[900px] flex flex-col gap-[20px] noxs1000:p-[20px]'>
                     <div className={`py-[20px] pb-[0px] flex justify-between noxs658:flex-col noxs1000:gap-[20px] items-center`}>
                         <p className="text-2xl font-bold noxs658:text-center">FAQs</p>
@@ -79,15 +79,19 @@ const Home = () => {
 
 
 
-            <div className="w-full mb-[150px] mt-[50px] flex justify-center">
+            <div className="w-full flex justify-center">
                 <div className='container max-w-[900px] flex flex-col gap-[20px] noxs1000:p-[20px]'>
                     {/* <div className={`py-[20px] pb-[0px] flex justify-between noxs658:flex-col noxs1000:gap-[20px] items-center`}>
                         <p className="text-2xl font-bold noxs658:text-center">FAQs</p>
                     </div> */}
-                    <div className="flex w-full">
-                        <video src="/1.mp4" controls autoPlay></video>
-                        <video src="/2.mp4" controls autoPlay></video>
-                    </div>  
+                    <div className="flex w-full gap-[50px] noxs658:flex-col">
+                        <div className="w-full rounded-2xl overflow-hidden">
+                            <video src="https://firebasestorage.googleapis.com/v0/b/samkhm34.appspot.com/o/1.mp4?alt=media&token=f05b62d1-e6c6-4ce2-b6a6-8127d30c73ea" controls className="w-full"></video>
+                        </div>
+                        <div className="w-full rounded-2xl overflow-hidden">
+                            <video src="https://firebasestorage.googleapis.com/v0/b/samkhm34.appspot.com/o/2.mp4?alt=media&token=710cf9f7-4db9-4188-94c0-738e53464ed5" controls className="w-full"></video>
+                        </div>
+                    </div>
                 </div>
             </div>
 
